75a30c1bf50107f8a9863edacbd1bd2b4ad843fa
Alex Guerrero <alexguerrero1092@gmail.com>
2015-12-13 01:13:58 +0100

    Remove semicolons due they are unnecesary in CocoaScript

--- Commands.sketchplugin/Contents/Sketch/library/common.js
+++ Commands.sketchplugin/Contents/Sketch/library/common.js
@@ -1,54 +1,54 @@
-var com = {};
+var com = {}
 
 com.bomberstudios = {
   alert: function (msg, title) {
-    if (title == undefined) { title = "Whoops" };
-    var app = [NSApplication sharedApplication];
-    [app displayDialog:msg withTitle:title];
+    if (title == undefined) { title = "Whoops" }
+    var app = [NSApplication sharedApplication]
+    [app displayDialog:msg withTitle:title]
   },
   create_folder: function(path) {
-    var file_manager = [NSFileManager defaultManager];
-    [file_manager createDirectoryAtPath:path withIntermediateDirectories:true attributes:nil error:nil];
+    var file_manager = [NSFileManager defaultManager]
+    [file_manager createDirectoryAtPath:path withIntermediateDirectories:true attributes:nil error:nil]
   },
   getFileFolder: function(doc){
     var file_url = [doc fileURL],
         file_path = [file_url path],
-        file_folder = file_path.split([doc displayName])[0];
-    return file_folder;
+        file_folder = file_path.split([doc displayName])[0]
+    return file_folder
   },
   getExportPath: function(doc){
     var file_folder = com.bomberstudios.getFileFolder(doc),
-        export_folder = file_folder + ([doc displayName]).split('.sketch')[0] + "_export/";
-    return export_folder;
+        export_folder = file_folder + ([doc displayName]).split('.sketch')[0] + "_export/"
+    return export_folder
   },
   export_all_slices: function(format,path, doc){
     log("com.bomberstudios.export_all_slices()")
     if (path == undefined) {
-      path = com.bomberstudios.getExportPath(doc);
+      path = com.bomberstudios.getExportPath(doc)
     }
 
     function export_loop() {
-      var pages = [doc pages];
+      var pages = [doc pages]
       for(var i=0; i < [pages count]; i++){
         var page = [pages objectAtIndex:i]
-        [doc setCurrentPage:page];
+        [doc setCurrentPage:page]
         var pagename = [[doc currentPage] name],
-            layers = [[doc currentPage] exportableLayers];
+            layers = [[doc currentPage] exportableLayers]
 
         for (var j=0; j < [layers count]; j++) {
           var slice = [layers objectAtIndex:j]
-          [doc saveArtboardOrSlice:slice toFile:path + "/" + pagename + "/" + [slice name] + "." + format];
+          [doc saveArtboardOrSlice:slice toFile:path + "/" + pagename + "/" + [slice name] + "." + format]
         }
       }
     }
 
-    export_loop();
+    export_loop()
   },
   export_all_artboards: function(format,path, doc){
     if (path == undefined) {
-      path = com.bomberstudios.getExportPath(doc);
+      path = com.bomberstudios.getExportPath(doc)
     }
-    log("com.bomberstudios.export_all_artboards() to " + path);
+    log("com.bomberstudios.export_all_artboards() to " + path)
 
     function export_loop() {
       var pages = [doc pages]
@@ -58,118 +58,118 @@ com.bomberstudios = {
         var pagename = [[doc currentPage] name],
             layers = [[doc currentPage] artboards]
 
-        log("Exporting page " + (i+1) + "/" + [pages count] + " (" + pagename + ") with " + [layers count] + " artboards");
+        log("Exporting page " + (i+1) + "/" + [pages count] + " (" + pagename + ") with " + [layers count] + " artboards")
 
         for (var j=0; j < [layers count]; j++) {
           var artboard = [layers objectAtIndex:j]
-          [doc saveArtboardOrSlice:artboard toFile:path + "/" + pagename + "/" + [artboard name] + "." + format];
+          [doc saveArtboardOrSlice:artboard toFile:path + "/" + pagename + "/" + [artboard name] + "." + format]
         }
       }
     }
 
-    export_loop();
+    export_loop()
   },
   export_item: function(item,format,path, doc){
-    var sel = item;
-    var rect = [sel absoluteInfluenceRect];
-    [doc saveArtboardOrSlice:[GKRect rectWithRect:rect] toFile:path + "/" + [sel name] + "." + format];
+    var sel = item
+    var rect = [sel absoluteInfluenceRect]
+    [doc saveArtboardOrSlice:[GKRect rectWithRect:rect] toFile:path + "/" + [sel name] + "." + format]
   },
   export_item_to_desktop: function(item,format, doc){
-    var desktop = [NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, true) objectAtIndex:0];
-    com.bomberstudios.export_item(item,format,desktop, doc);
+    var desktop = [NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, true) objectAtIndex:0]
+    com.bomberstudios.export_item(item,format,desktop, doc)
   },
   padNumber: function(num){
-    num = num.toString();
+    num = num.toString()
     if (num.length < 2) {
-      num = "0" + num;
-    };
-    return num;
+      num = "0" + num
+    }
+    return num
   },
   isodate: function(){
-    var now = new Date();
-    var year = now.getFullYear();
-    var month = com.bomberstudios.padNumber((now.getMonth() + 1));
-    var day = com.bomberstudios.padNumber(now.getDate());
-    var hour = com.bomberstudios.padNumber(now.getHours());
-    var minute = com.bomberstudios.padNumber(now.getMinutes());
-    return year + month + day + hour + minute;
+    var now = new Date()
+    var year = now.getFullYear()
+    var month = com.bomberstudios.padNumber((now.getMonth() + 1))
+    var day = com.bomberstudios.padNumber(now.getDate())
+    var hour = com.bomberstudios.padNumber(now.getHours())
+    var minute = com.bomberstudios.padNumber(now.getMinutes())
+    return year + month + day + hour + minute
   },
   selection_count_is: function(min){
     var min = min || 1,
-        title = "Whoops";
+        title = "Whoops"
     if ([selection count] < min) {
       if([selection count] === 0) {
         title = "Nihilism alert"
       }
       alert("You need to select at least " + number_to_words(min) + " object" + (min === 1 ? '' : 's') + ", but you selected " + number_to_words([selection count]) + ".", title)
-      return false;
+      return false
     } else {
-      return true;
+      return true
     }
   },
   number_to_words: function(num){
     switch (num){
       case 0:
-        return 'none';
+        return 'none'
       case 1:
-        return 'one';
+        return 'one'
       case 2:
-        return 'two';
+        return 'two'
       case 3:
-        return 'three';
+        return 'three'
       case 4:
-        return 'four';
+        return 'four'
       default:
-        return num;
+        return num
     }
   },
   open_finder_in: function(path) {
     var open_finder = [[NSTask alloc] init],
-        open_finder_args = [NSArray arrayWithObjects:".", nil];
+        open_finder_args = [NSArray arrayWithObjects:".", nil]
 
-    [open_finder setCurrentDirectoryPath:path];
-    [open_finder setLaunchPath:"/usr/bin/open"];
-    [open_finder setArguments:open_finder_args];
-    [open_finder launch];
+    [open_finder setCurrentDirectoryPath:path]
+    [open_finder setLaunchPath:"/usr/bin/open"]
+    [open_finder setArguments:open_finder_args]
+    [open_finder launch]
   },
   reveal_finder_in: function(path) {
     var open_finder = [[NSTask alloc] init],
-        open_finder_args = [NSArray arrayWithObjects:"-R", path, nil];
+        open_finder_args = [NSArray arrayWithObjects:"-R", path, nil]
 
-    [open_finder setLaunchPath:"/usr/bin/open"];
-    [open_finder setArguments:open_finder_args];
-    [open_finder launch];
+    [open_finder setLaunchPath:"/usr/bin/open"]
+    [open_finder setArguments:open_finder_args]
+    [open_finder launch]
   }
-};
+}
 
 Array.prototype.each = function(callback){
-  var count = 0;
+  var count = 0
   for (var i = 0; i < this.length; i++){
-    var el = this[i];
-    callback.call(this,el,count);
-    count++;
+    var el = this[i]
+    callback.call(this,el,count)
+    count++
   }
 }
 
 Number.prototype.times = function(callback){
   for (var s = this - 1; s >= 0; s--){
-    callback.call(this,s);
-  };
+    callback.call(this,s)
+  }
 }
 
 Date.prototype.isoDate = function(){
-  var d = this;
-  return d.year();
+  var d = this
+  return d.year()
 }
 
 function toJSArray(arr) {
   var len = [arr count],
-      res = [];
+      res = []
 
   while(len--){
-    res.push(arr[len]);
+    res.push(arr[len])
   }
-  return res;
+  return res
 }
 
 
